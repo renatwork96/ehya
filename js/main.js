f0f3574966831ba3d8be1bf4b101218feb4c0096
renatwork96 <renat.work96@gmail.com>
2021-04-03 15:01:23 +0300

    create: modal form

--- js/main.js
+++ js/main.js
@@ -61,3 +61,56 @@
     },
   });
 
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  var closeModalOverlay = $(".modal__overlay");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+  closeModalOverlay.on("click", closeModal);
+
+  $(document).keyup(function(e) {
+	if (e.key === "Escape" || e.keyCode === 27) {
+		var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+  });
+
+  function openModal() {
+    
+    var targetModal = $(this).attr('data-href');
+    console.log(targetModal);
+    $(targetModal).find(".modal__overlay").addClass('modal__overlay--visible');
+    $(targetModal).find(".modal__dialog").addClass('modal__dialog--visible');
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+  // Обработка форм
+  $(".form").each(function(){
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "The name must be at least two letters"
+        },
+        email: {
+          required: "Please specify your email",
+          email: "Your email address must be in the format of name@domain.com"
+        },
+        phone: {
+          required: "Please specify your phone number",
+          minlength: "The phone must be at least ten digits"
+        },
+      },
+    });
+  })
+  $(".phone").mask("+7 (999) 999-99-99");
+
---
+++ send.php
@@ -0,0 +1,62 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+// Формирование самого письма
+$title = "New appeal Best Tour Plan";
+$body = "
+<h2>New appeal</h2>
+<b>Name:</b> $name<br>
+<b>Phone number:</b> $phone<br>
+<b>E-mail:</b> $email<br><br>
+<b>Message:</b><br>$message
+";
+
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+     
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'renatbiz.mail@gmail.com'; // Логин на почте
+    $mail->Password   = 'sIpwTZaY'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('renatbiz.mail@gmail.com', 'Khakov Renat'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('chakov_renat@mail.ru');
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone'])) {
+        $mail->Body = $body; 
+    } 
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');
